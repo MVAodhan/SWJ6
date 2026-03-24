6c7c4cece7492b776655012363a8896987fc6330
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2026-03-24 20:32:58 +1300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
+import path from "path";
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
